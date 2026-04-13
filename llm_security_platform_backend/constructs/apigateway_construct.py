5ab900d465ff86096d400bc1647d18e63b697492
rudikko <168979965+rudikko@users.noreply.github.com>
2026-04-13 22:49:27 +0300

    update apigw

--- llm_security_platform_backend/constructs/apigateway_construct.py
+++ llm_security_platform_backend/constructs/apigateway_construct.py
@@ -6,21 +6,24 @@ class ApiGatewayConstruct(Construct):
     def __init__(self, scope: Construct, id: str, lambda_functions: dict):
         super().__init__(scope, id)
 
+
         self.api = apigateway.RestApi(
             self, "LLmSecurityPlatformApiGW",
             rest_api_name="LLmSecurityPlatformApiGW",
-            deploy_options=apigateway.StageOptions(stage_name="dev")
+            deploy_options=apigateway.StageOptions(stage_name="prod")
         )
+
+
 
         # /admin
         admin_resource = self.api.root.add_resource("admin")
-        admin_resource.add_method(
-            "POST",
-            apigateway.LambdaIntegration(lambda_functions["create_challenge_lambda"])
-        )
 
         # /admin/challenges
         admin_challenges_resource = admin_resource.add_resource("challenges")
+        admin_challenges_resource.add_method(
+            "GET",
+            apigateway.LambdaIntegration(lambda_functions["list_owner_challenges_lambda"])
+        )
         admin_challenges_resource.add_method(
             "POST",
             apigateway.LambdaIntegration(lambda_functions["create_challenge_lambda"])
@@ -61,6 +64,8 @@ class ApiGatewayConstruct(Construct):
             "GET",
             apigateway.LambdaIntegration(lambda_functions["get_challenge_lambda"])
         )
+
+        # /challenges/{challenge_id}/start
         challenge_id_resource.add_resource("start").add_method(
             "POST",
             apigateway.LambdaIntegration(lambda_functions["start_challenge_lambda"])
@@ -68,11 +73,13 @@ class ApiGatewayConstruct(Construct):
 
         # /sessions
         sessions_resource = self.api.root.add_resource("sessions")
+        # /sessions/{session_id}
         session_id_resource = sessions_resource.add_resource("{session_id}")
         session_id_resource.add_method(
             "GET",
             apigateway.LambdaIntegration(lambda_functions["poll_for_responses_lambda"])
         )
+        # /sessions/{session_id}/messages
         session_id_resource.add_resource("messages").add_method(
             "POST",
             apigateway.LambdaIntegration(lambda_functions["send_message_to_queue_lambda"])
